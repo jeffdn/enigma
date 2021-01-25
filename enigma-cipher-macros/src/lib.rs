9b5776254326f53395be8ef2518d40561b92301d
Jeff Nettleton <jeffdn@gmail.com>
2021-01-25 11:23:53 -0800

    Implement Reflector as a derivable trait, add remaining Heer reflectors and rotors

--- enigma-cipher-macros/src/lib.rs
+++ enigma-cipher-macros/src/lib.rs
@@ -1,3 +1,12 @@
+// src/enigma-cipher-macros/lib.rs
+//
+// Copyright (c) 2021
+// Jeff Nettleton
+//
+// Licensed under the MIT license (http://opensource.org/licenses/MIT). This
+// file may not be copied, modified, or distributed except according to those
+// terms
+
 extern crate proc_macro;
 
 use std::collections::HashSet;
@@ -7,18 +16,18 @@ use proc_macro2;
 use quote::quote;
 use syn;
 
-#[proc_macro_derive(RotorEncode, attributes(key_ordering, notches))]
-pub fn rotor_encode_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-
-    impl_rotor_encode(&ast)
-}
-
 fn extract_attribute(tokens: &proc_macro2::TokenStream) -> String {
     let attr_str = &tokens.to_string();
     let to_trim: &[_] = &['(', ')'];
 
     attr_str.trim_matches(to_trim).into()
+}
+
+#[proc_macro_derive(RotorEncode, attributes(key_ordering, notches))]
+pub fn rotor_encode_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_rotor_encode(&ast)
 }
 
 fn impl_rotor_encode(ast: &syn::DeriveInput) -> TokenStream {
@@ -73,26 +82,46 @@ fn impl_rotor_encode(ast: &syn::DeriveInput) -> TokenStream {
                 }
             }
 
-            fn transpose_in(&self, input: char) -> char {
-                let offset_input = _shift_char_offset(input, self.get_offset() * -1);
+            fn _apply_offset(&self, shifted: i8) -> char {
+                if shifted > 90 {
+                    return ((shifted - 26) as u8) as char;
+                } else if shifted < 65 {
+                    return ((shifted + 26) as u8) as char;
+                } else {
+                    return (shifted as u8) as char;
+                }
+            }
 
-                let computed = match offset_input as char {
+            fn _shift_input(&self, input: char) -> char {
+                let offset: i8 = self.get_offset() * -1;
+                let input_val = input as i8;
+
+                self._apply_offset(input_val + offset)
+            }
+
+            fn _shift_output(&self, output: char) -> char {
+                let offset: i8 = self.get_offset();
+                let output_val = output as i8;
+
+                self._apply_offset(output_val + offset)
+            }
+
+            fn transpose_in(&self, input: char) -> char {
+                let computed = match self._shift_input(input) {
                     #transpose_in
                     _  => ' ',
                 };
 
-                _shift_char_offset(computed, self.get_offset())
+                self._shift_output(computed)
             }
 
             fn transpose_out(&self, input: char) -> char {
-                let offset_input = _shift_char_offset(input, self.get_offset() * -1);
-
-                let computed = match offset_input as char {
+                let computed = match self._shift_input(input) {
                     #transpose_out
                     _  => ' ',
                 };
 
-                _shift_char_offset(computed, self.get_offset())
+                self._shift_output(computed)
             }
 
             fn at_notch(&self) -> bool {
@@ -112,7 +141,7 @@ fn impl_rotor_encode(ast: &syn::DeriveInput) -> TokenStream {
             }
 
             fn position(&self) -> char {
-                _shift_char_offset(self.ring_setting, self.get_offset() * -1)
+                self._shift_input(self.ring_setting)
             }
 
             fn get_offset(&self) -> i8 {
@@ -122,6 +151,55 @@ fn impl_rotor_encode(ast: &syn::DeriveInput) -> TokenStream {
                     return (self.init_offset - self.cur_offset) as i8;
                 } else {
                     return ((self.init_offset + 26) - self.cur_offset) as i8;
+                }
+            }
+        }
+    };
+
+    gen.into()
+}
+
+#[proc_macro_derive(Reflector, attributes(key_ordering))]
+pub fn reflector_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_reflector(&ast)
+}
+
+fn impl_reflector(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let mut key_ordering: String = "empty".into();
+
+    for attr in ast.attrs.iter() {
+        let attr_name = attr.path.get_ident().unwrap().to_string();
+        match &*attr_name {
+            "key_ordering" => key_ordering = extract_attribute(&attr.tokens),
+            _ => {},
+        };
+    }
+
+    let key_tokens: Vec<char> = key_ordering.chars().collect();
+    let key_space: HashSet<char> = HashSet::from_iter(key_tokens.clone().into_iter());
+
+    if key_space.len() != 26 {
+        assert!(false, "Expected 26 unique characters in #[key_ordering(...)]");
+    }
+
+    let mut transpose: proc_macro2::TokenStream = quote!();
+
+    for (i, x) in key_tokens.into_iter().enumerate() {
+        let mapped_char = ((i + 65) as u8) as char;
+        transpose.extend(quote! {
+            #mapped_char => #x,
+        });
+    }
+
+    let gen = quote! {
+        impl Reflector for #name {
+            fn transpose(&self, input: char) -> char {
+                match input {
+                    #transpose
+                    _  => ' ',
                 }
             }
         }
--- src/rotors.rs
+++ src/rotors.rs
@@ -1,75 +1,41 @@
 // src/rotors.rs
 //
-// Copyright (c) 2018
+// Copyright (c) 2021
 // Jeff Nettleton
 //
 // Licensed under the MIT license (http://opensource.org/licenses/MIT). This
 // file may not be copied, modified, or distributed except according to those
 // terms
 
-use enigma_cipher_macros::RotorEncode;
+use enigma_cipher_macros::{Reflector, RotorEncode};
 
-fn _shift_char_offset(input: char, offset: i8) -> char {
-    let input_val = input as i8;
-    let shifted: i8 = input_val + offset;
-
-    if shifted > 90 {
-        return ((shifted - 26) as u8) as char;
-    } else if shifted < 65 {
-        return ((shifted + 26) as u8) as char;
-    } else {
-        return (shifted as u8) as char;
-    }
+pub trait Reflector {
+    fn transpose(&self, input: char) -> char;
 }
+
+#[derive(Reflector)]
+#[key_ordering(EJMZALYXVBWFCRQUONTSPIKHGD)]
+pub struct ReflectorA;
+
+#[derive(Reflector)]
+#[key_ordering(YRUHQSLDPXNGOKMIEBFZCWVJAT)]
+pub struct ReflectorB;
+
+#[derive(Reflector)]
+#[key_ordering(FVPJIAOYEDRZXWGCTKUQSBNMHL)]
+pub struct ReflectorC;
 
 pub trait RotorEncode {
     fn new(ring_setting: char, init_position: char) -> Self;
+    fn _apply_offset(&self, shifted: i8) -> char;
+    fn _shift_input(&self, input: char) -> char;
+    fn _shift_output(&self, output: char) -> char;
     fn transpose_in(&self, input: char) -> char;
     fn transpose_out(&self, input: char) -> char;
     fn advance(&mut self);
     fn position(&self) -> char;
     fn get_offset(&self) -> i8;
     fn at_notch(&self) -> bool;
-}
-
-pub trait Reflector {
-    fn transpose(&self, input: char) -> char;
-}
-
-pub struct ReflectorB;
-
-impl Reflector for ReflectorB {
-    fn transpose(&self, input: char) -> char {
-        match input {
-            'A' => 'Y',
-            'B' => 'R',
-            'C' => 'U',
-            'D' => 'H',
-            'E' => 'Q',
-            'F' => 'S',
-            'G' => 'L',
-            'H' => 'D',
-            'I' => 'P',
-            'J' => 'X',
-            'K' => 'N',
-            'L' => 'G',
-            'M' => 'O',
-            'N' => 'K',
-            'O' => 'M',
-            'P' => 'I',
-            'Q' => 'E',
-            'R' => 'B',
-            'S' => 'F',
-            'T' => 'Z',
-            'U' => 'C',
-            'V' => 'W',
-            'W' => 'V',
-            'X' => 'J',
-            'Y' => 'A',
-            'Z' => 'T',
-            _   => ' ',
-        }
-    }
 }
 
 #[derive(RotorEncode)]
@@ -94,6 +60,24 @@ pub struct RotorII {
 #[key_ordering(BDFHJLCPRTXVZNYEIWGAKMUSQO)]
 #[notches(V)]
 pub struct RotorIII {
+    ring_setting: char,
+    init_offset: u8,
+    cur_offset: u8,
+}
+
+#[derive(RotorEncode)]
+#[key_ordering(ESOVPZJAYQUIRHXLNFTGKDCMWB)]
+#[notches(J)]
+pub struct RotorIV {
+    ring_setting: char,
+    init_offset: u8,
+    cur_offset: u8,
+}
+
+#[derive(RotorEncode)]
+#[key_ordering(VZBRGITYUPSDNHLXAWMJQOFECK)]
+#[notches(Z)]
+pub struct RotorV {
     ring_setting: char,
     init_offset: u8,
     cur_offset: u8,
